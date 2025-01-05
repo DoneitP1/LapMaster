be19fce9656781e34633756e9b00069e697db1e9
doneitp1 aka ee29 <145833591+DoneitP1@users.noreply.github.com>
2025-01-05 15:32:19 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    MYSQL_HOST = 'localhost'
-    MYSQL_USER = 'yourusername'
-    MYSQL_PASSWORD = 'yourpassword'
-    MYSQL_DB = 'f1telemetry'
-    MYSQL_CURSORCLASS = 'DictCursor'
-    CACHE_DIR = os.path.join(os.getcwd(), 'cache')
